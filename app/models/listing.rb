e1a790c6ee2b73dcdb4782d9478d832940cf629d
Samseppiol <j_hage@live.com>
2017-05-06 15:04:14 +1000

    Added paperclip and image magick, added another column to the listings database for images. Updated all the views for images

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,4 @@
 class Listing < ApplicationRecord
+  has_attached_file :image, styles: { medium: "250x250>", thumb: "100x100>", default_url: "250x250>"  }, default_url: "missing.jpg"
+ validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506034323) do
+ActiveRecord::Schema.define(version: 20170506042434) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.decimal  "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
