ebf3042d996c2bf32aa56f09467265c2233921cb
Samseppiol <j_hage@live.com>
2017-05-07 12:28:14 +1000

    Added first and last names to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root 'pages#index'
   resources :listings
   resources :pages, only: [:index]
