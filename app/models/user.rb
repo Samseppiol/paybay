b781a1ac3ec6d07bf8bda27a168ee05655c0b09b
Samseppiol <j_hage@live.com>
2017-05-07 13:22:39 +1000

    Added validations for new user input fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :first_name, :last_name, :date_of_birth, presence: true
+  validates_length_of :first_name, :last_name, { :minimum =>2, :maximum => 25}
 end
