0f58faa09e4caea86b11cc619ec9372293683ca6
Samseppiol <j_hage@live.com>
2017-05-07 13:15:33 +1000

    All my tests were mysteriously failing after installing devise. It was as simple as changing the test fixture data so the emails weren't the same, because rails was having a meltdown over email uniqueness

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170507022829) do
+ActiveRecord::Schema.define(version: 20170507011055) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -39,7 +39,6 @@ ActiveRecord::Schema.define(version: 20170507022829) do
     t.datetime "updated_at",                          null: false
     t.string   "first_name"
     t.string   "last_name"
-    t.datetime "date_of_birth"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
