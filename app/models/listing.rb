fd60217b6a63083809e7f58d69bfd4b18b6124e7
Samseppiol <j_hage@live.com>
2017-05-07 14:39:40 +1000

    After I reset the database like a moron, I just found out paperclip was not uploading pictures. It seems something I did messed with paperclip and its taken me 30 minutes to fix it, fun times

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,11 +2,11 @@ class Listing < ApplicationRecord
   validates :name, :description, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :name, uniqueness: true
-  validates :image_file_name, allow_blank: true, format: {
-    with: %r{\.(gif|jpg|jpeg|png)\Z}i,
-    message: 'Must be a url for a gif, jpg or png image file.'
-  }
+  # validates :image_file_name, allow_blank: true, format: {
+  #   with: %r{\.(gif|jpg|jpeg|png)\Z}i,
+  #   message: 'Must be a url for a gif, jpg or png image file.'
+  # }
 
-  has_attached_file :image, styles: { medium: "250x250>", thumb: "100x100>", default_url: "250x250>"  }, default_url: "missing.jpg"
+  has_attached_file :image, styles: { medium: "250x250>", thumb: "100x100>" }, default_url: "missing.jpg"
  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
