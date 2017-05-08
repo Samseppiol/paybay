0b4c5ee85043c18893ce99ebbfd3b0ef431e73d5
Samseppiol <j_hage@live.com>
2017-05-08 16:02:00 +1000

    Wrote a few tests for orders, pretty proud of getting this to work. Rails makes it super easy, write a test and if you dont know the error message the validation should show you simply fail the test on purpose and rails will tell you what the error message should be

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,6 @@ class Order < ApplicationRecord
   validates :address, :city, :state, :postcode, presence: true
   validates_numericality_of :postcode, :only_integer => true, :allow_nil => true
   validates_length_of :postcode, {:minimum => 4, :maximum => 4}
+  validates_length_of :address, :city, :state, {:minimum => 2, :maximum => 50}
 
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Order attributes must not be empty' do
+    order = Order.new
+    assert order.invalid?
+    assert order.errors[:address].any?
+    assert order.errors[:city].any?
+    assert order.errors[:state].any?
+    assert order.errors[:postcode].any?
+  end
+
+  test 'Postcode is valid' do
+    order = Order.new(address: '123 fake street',
+                      city: 'Fakebourne',
+                      state: 'vic')
+    order.postcode = '321'
+    assert order.invalid?
+    assert_equal ["is too short (minimum is 4 characters)"], order.errors[:postcode]
+    order.postcode = 'abcd'
+    assert order.invalid?
+    assert_equal ["is not a number", "is too short (minimum is 4 characters)"], order.errors[:postcode]
+
+    order.postcode = '3223'
+    assert order.valid?
+  end
+
+
+  test 'City name is valid' do
+    order = Order.new(address: '123 fake street',
+                      state: 'vic',
+                      postcode: '3198')
+    order.city = 'a'
+    assert order.invalid?
+    assert_equal ["is too short (minimum is 2 characters)"], order.errors[:city]
+
+    order.city = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
+    assert order.invalid?
+    assert_equal ["is too long (maximum is 50 characters)"], order.errors[:city]
+
+    order.city = 'melbourne'
+    assert order.valid?
+  end
+
+
 end
