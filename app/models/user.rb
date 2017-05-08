a05089eb7797e112419535fe1cb898c568ddf193
Samseppiol <j_hage@live.com>
2017-05-08 11:43:46 +1000

    Added validations for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :listings, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :first_name, :last_name, :date_of_birth, presence: true
-  validates_length_of :first_name, :last_name, { :minimum =>2, :maximum => 25}
+  validates :first_name, :last_name, :date_of_birth, :username, presence: true
+  validates_length_of :first_name, :last_name, :username, { :minimum =>2, :maximum => 25}
+  validates :username, uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,5 +7,17 @@ class UserTest < ActiveSupport::TestCase
     assert user.errors[:first_name].any?
     assert user.errors[:last_name].any?
     assert user.errors[:date_of_birth].any?
+    assert user.errors[:username].any?
   end
+
+
+  test "Password must be valid" do
+    user = User.new(
+      email: 'test@example.com',
+      password: 'password',
+      password_confirmation: 'password'
+    )
+    assert(user.valid_password?('password'))
+  end
+
 end
