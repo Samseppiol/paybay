e6a2d269bedbd125c19581a53632be76990c9eab
Samseppiol <j_hage@live.com>
2017-05-11 14:44:12 +1000

    Changed controller info

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,6 +1,7 @@
 class ConfirmationsController < Devise::ConfirmationsController
   private
   def after_confirmation_path_for(resource_name, resource)
-    your_new_after_confirmation_path
+    flash[:notice] = "Thank you for confirming your email"
+      root_path
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
 }
 
 config.action_mailer.delivery_method = :smtp
-config.action_mailer.default_url_options = { :host => 'https://morning-stream-13193.herokuapp.com/', :protocol => 'https'}
+config.action_mailer.default_url_options = { :host => 'https://morning-stream-13193.herokuapp.com/'}
 
   # Code is not reloaded between requests.
   config.cache_classes = true
