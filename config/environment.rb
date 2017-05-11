ce78100feb815a61266a9556af9f65ed325ab936
Samseppiol <j_hage@live.com>
2017-05-11 14:23:07 +1000

    updated mailer

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
-  :address => 'smptp.sendgrid.net',
+  :address => 'smtp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
   :user_name => ENV['SENDGRID_USERNAME'],
